1801374083f2c22e97652c3c32ee1b2a2bc9f06b
hmtheboy154 <buingoc67@gmail.com>
2023-05-22 16:27:59 -0400

    Only include iio and kbd sensors
    Since those HAL are for the old tablets that are even 32bit, it's time
    to remove them and move on
    
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- sensors.mk
+++ sensors.mk
@@ -1,9 +1,5 @@
 # Copyright (C) 2013 The Android-x86 Open Source Project
 
 PRODUCT_PACKAGES := \
-    sensors.hdaps \
     sensors.iio \
-    sensors.iio-old \
-    sensors.kbd \
-    sensors.s103t \
-    sensors.w500 \
+    sensors.kbd
